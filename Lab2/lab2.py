7ba8a9815bab22211250870e93c14c167960393e
Evgenii Baldin <evgenii.baldin0@gmail.com>
2025-01-14 10:56:20 -0500

    Rolling elements added

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,3 +1,5 @@
+from random import randint
+
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("Elements: ", *elements)
 
@@ -15,3 +17,19 @@ def get_valid_int_input(prompt):
         except ValueError:
             print("Please enter a valid integer value.")
             continue
+
+try:
+    element_selected = get_valid_int_input("Enter the number of elements you want to select: ")
+
+    # Roll dice
+    element_roll = randint(0, len(elements)-1)
+    total_num = element_selected + element_roll
+
+    if element_roll <= 2:
+        print("You rolled a weak element")
+    elif element_roll <= 4:
+        print("You rolled a moderate element")
+    else:
+        print("Nice element.")
+except Exception as e:
+    pass
