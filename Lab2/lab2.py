16da5c2cc984bc785af8c40cd1faeeeb2ce67346
Evgenii Baldin <evgenii.baldin0@gmail.com>
2025-01-14 10:53:36 -0500

    input function added

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -1,2 +1,17 @@
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("Elements: ", *elements)
+
+# def funct_name():
+#     return True
+# def say_greeting(name, message="hi"):
+#     print(f" {message}, {name}")
+# say_greeting("Maziar")
+# say_greeting("Maziar", "Hello")
+
+def get_valid_int_input(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Please enter a valid integer value.")
+            continue
